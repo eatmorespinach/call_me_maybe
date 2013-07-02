e146a48b8b40192a714489da5fdd44dfc5199819
Drew Sing <sing.drew@gmail.com>
2013-07-02 13:36:53 -0400

    call me maybe first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CallMeMaybe::Application.config.secret_token = 'd36c3c47a78c431b48172a2533ba65ca595815f83f380669bd7eec6caeab32ce98b8d42f8a345b2453f2b69ff84ed7f082d8859ef270ff0c75cae29d234bcdf0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CallMeMaybe::Application.config.session_store :cookie_store, key: '_call_me_maybe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CallMeMaybe::Application.config.session_store :active_record_store
